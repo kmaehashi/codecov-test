bdebee8a5ec7125382992ca4b2b0bdedf0ddf638
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-02-22 15:56:01 +0900

    add code

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import unittest
+import sys
+
+from mylib.app import App
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(3, App().add(1, 2))
+
+    def test_sub(self):
+        if sys.version_info[0] == 2:
+            self.assertEqual(-1, App().sub(1, 2))
